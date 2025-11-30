bc59de3d61f477242a9024b28a422ba3dc31894d
Abdimalik <binabdimalikranger@gmail.com>
2025-11-30 20:39:58 +0300

    resolved

--- lib/relative_package/subpackage1/subpackage2/module2.py
+++ lib/relative_package/subpackage1/subpackage2/module2.py
@@ -1,5 +1,10 @@
 from .module1 import function1
 # from .. import module3
+from .. import module3
 
-function1()
+if __name__ == "__main__":
+    print("--- Executing Relative Imports ---")
+
+    function1()
 # module3.function1()
+    module3.function1
